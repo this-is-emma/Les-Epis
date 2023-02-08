e56f21b74e77d7d5fa48d69f0f71ff201aa22a26
ThisIsEmma <sakatia.lise@gmail.com>
2023-02-07 21:09:20 -0500

    The beginning of a great story

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+// src/Title.js
+import React from "react";
+import './Title.css';
+
+function Title() {
+    return (
+        <>
+            <div className="Title">
+                <h1>FONDATION LES EPIS</h1> 
+            </div>
+            <div className="Title-Subtitle">
+                <h3>Help us give back to communities</h3>
+            </div>
+        </>
+    )
+  }
+  
+  export default Title
